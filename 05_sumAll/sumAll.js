a501b67131aebc924beb456a6f5cf913b2bce343
w2904 <153371554+w2904@users.noreply.github.com>
2024-06-15 23:34:28 +0000

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    const biggerNumber = (num2 > num1) ? num2 : num1;
+    const smallerNumber = (num2 > num1) ? num1 : num2;
+
+    let finalSum = 0;
+    for (let i = smallerNumber; i <= biggerNumber; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
