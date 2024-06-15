38d3a7c343974a77384dc48800a998f948e76fa1
w2904 <153371554+w2904@users.noreply.github.com>
2024-06-15 23:05:16 +0000

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...arrayOfElements) {
+    for (let i = 0; i < arrayOfElements.length; i++) {
+        for (let j = 0; j < mainArray.length; j++) {
+            if (arrayOfElements[i] === mainArray[j]) {
+                    mainArray.splice(j, 1);
+                    j -= 1;
+                    continue;
+            }
+        }
+    }
+    return mainArray;
 };
 
 // Do not edit below this line
