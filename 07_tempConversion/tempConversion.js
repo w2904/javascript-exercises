6274fa000f293edbe47d8a7f4c23d81d830c32e7
w2904 <153371554+w2904@users.noreply.github.com>
2024-06-16 00:09:43 +0000

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  celsius = (degree - 32) * 5 / 9;
+  return Number.isInteger(celsius) ? (celsius) : +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  fahrenheit = degree * 9 / 5 + 32;
+  return Number.isInteger(fahrenheit) ? (fahrenheit) : +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
