65a90a24b4213e534424508d4da38d6d34eafab8
w2904 <153371554+w2904@users.noreply.github.com>
2024-08-04 22:20:12 +0000

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sortedArray = arr.sort((a, b) => {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    })
+    return sortedArray[0];
 };
 
 // Do not edit below this line
