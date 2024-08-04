5af554dddc8bc542f588ee86e1ec20a0c479be65
w2904 <153371554+w2904@users.noreply.github.com>
2024-08-04 21:36:46 +0000

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(numPosition) {
+    if (+numPosition === 0) {
+        return 0;
+    }
+    else if (+numPosition < 0) {
+        return "OOPS";
+    }
+    let [num1, num2, sum] = [1, 1, 1];
+    for (let i = 2; i < +numPosition; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
