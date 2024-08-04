848173ad606251f49e0aa9a5e67884653b66a27b
w2904 <153371554+w2904@users.noreply.github.com>
2024-08-04 21:07:45 +0000

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const splitString = string.split("");
+	splitString.reverse();
+	return splitString.join("") === string;
 };
 
 // Do not edit below this line
